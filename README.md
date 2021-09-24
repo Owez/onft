e1c4b643ecf496bc3f069137e864156cbe083036
Owez <root@ogriffiths.com>
2021-09-24 15:13:22 +0100

    Added licensing and README.md

---
+++ README.md
@@ -0,0 +1,7 @@
+# NFTZ
+
+Bespoke protocol and high-level implementation of Non-fungible token (NFT) technology 🚀
+
+## Licensing
+
+This project is duel-licensed under both the [MIT](https://en.wikipedia.org/wiki/MIT_License) and [Apache](https://en.wikipedia.org/wiki/Apache_License) licenses, so feel free to use either at your discretion.
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,12 @@
+//! Bespoke protocol and high-level implementation of Non-fungible token (NFT) technology 🚀
+//!
+//! # Licensing
+//!
+//! This project is duel-licensed under both the [MIT](https://en.wikipedia.org/wiki/MIT_License) and [Apache](https://en.wikipedia.org/wiki/Apache_License) licenses, so feel free to use either at your discretion.
+
+#![deny(unsafe_code)]
+#![warn(missing_docs)]
+
 mod block;
 mod error;
 mod hash;
